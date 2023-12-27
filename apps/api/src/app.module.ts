ee94d76a2b629867aa847783d60ad07b9a6a0012
Sunghum Paik (Brian) <zerosial@gmail.com>
2023-12-27 18:17:58 +0900

    feat: dist로 다시 변경

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -7,7 +7,7 @@ import { join } from 'path';
 @Module({
   imports: [
     ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '../..', 'client', 'public'),
+      rootPath: join(__dirname, '../..', 'client', 'dist'),
       exclude: ['api/*'],
     }),
   ],
