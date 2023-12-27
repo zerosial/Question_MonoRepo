885961c0a562e0d2893ad1ef4887dc002f2a6692
Sunghum Paik (Brian) <zerosial@gmail.com>
2023-12-27 17:32:14 +0900

    feat: vercel 추가 완료

--- apps/client/vite.config.js
+++ apps/client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "https://question-monorepo.vercel.app",
+        target: "http://localhost:3000",
         changeOrigin: true,
       },
     },
